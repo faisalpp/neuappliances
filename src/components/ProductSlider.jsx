85bca2e8abac0310644e7b0138e6894a5cd3b440
suneelkumar555 <k163997@nu.edu.pk>
2023-05-22 14:57:51 +0500

    slider changed

--- src/components/ProductSlider.jsx
+++ src/components/ProductSlider.jsx
@@ -10,10 +10,10 @@ const ProductSlider = () => {
     <CarouselProvider
     naturalSlideWidth={100}
     naturalSlideHeight={80}
-    totalSlides={2}
+    totalSlides={6}
     visibleSlides={1}
     >
-   <ButtonBack className='absolute top-28 left-8 z-40'><div className='hidden lg:flex bg-black/30 hover:bg-cyan-500 cursor-pointer px-2 py-2 rounded-full text-white group'><BsArrowLeftShort className='text-xl'/></div></ButtonBack>
+    <ButtonNext className='absolute top-28 left-8 z-40'><div className='hidden lg:flex bg-black/30 hover:bg-cyan-400 px-2 py-2 rounded-full text-white'><BsArrowLeftShort className='text-lg'/></div></ButtonNext>
     <Slider>
       <Slide index={0}>
         <img src="p1.png" className='w-full h-full' />
@@ -21,8 +21,20 @@ const ProductSlider = () => {
       <Slide index={1}>
         <img src="p1.png" className='w-full h-full' />
       </Slide>
+      <Slide index={2}>
+        <img src="p1.png" className='w-full h-full' />
+      </Slide>
+      <Slide index={3}>
+        <img src="p1.png" className='w-full h-full' />
+      </Slide>
+      <Slide index={4}>
+        <img src="p1.png" className='w-full h-full' />
+      </Slide>
+      <Slide index={5}>
+        <img src="p1.png" className='w-full h-full' />
+      </Slide>
     </Slider>
-    <ButtonNext className='absolute top-28 right-8 z-40'><div className='hidden lg:flex bg-black/30 hover:bg-cyan-400 px-2 py-2 rounded-full text-white'><BsArrowRightShort className='text-lg'/></div></ButtonNext>
+   <ButtonBack className='absolute top-28 right-8 z-40'><div className='hidden lg:flex bg-black/30 hover:bg-cyan-500 cursor-pointer px-2 py-2 rounded-full text-white group'><BsArrowRightShort className='text-xl'/></div></ButtonBack>
   </CarouselProvider>    
     </>
   )
