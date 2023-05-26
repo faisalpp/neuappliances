96c83b87c16f9e5b217baac61013bf1941f69368
suneelkumar-24 <k163997@nu.edu.pk>
2023-05-26 10:16:35 +0500

    font size changed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
     <>
           <div className='lg:grid flex flex-col lg:space-y-0 space-y-10 grid-cols-12 items-center py-20 lg:px-10 px-5 bg-b5 auto' >
           <div className='col-start-1 col-end-7 flex flex-col space-y-10' >
-           <h4 className='text-5xl font-bold' >Austin's Best Deals For Scratch & Dent Appliances</h4>
+           <h4 className='lg:text-5xl text-3xl lg:text-start text-center font-bold' >Austin's Best Deals For Scratch & Dent Appliances</h4>
            <div className='flex lg:justify-start justify-center' ><a href="#" className='flex text-white rounded-md space-x-2 font-semibold items-center justify-center bg-b7 w-56 h-[56px] text-sm' ><AiOutlineShoppingCart className='text-xl' /><span>Discover The Savings</span></a></div>
           </div>  
           <div className='col-start-8 col-end-13 flex justify-center' >
