f116561a1e0df25c833e825d885f9740af570fe3
suneelkumar-24 <k163997@nu.edu.pk>
2023-06-29 23:14:13 +0500

    ff

--- api/server.js
+++ api/server.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: '10mb' }));
 
 // Increase payload size limit for URL-encoded requests
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(express.json())
+app.use(express.json());
 
 app.use(cors({
   credentials: true,
