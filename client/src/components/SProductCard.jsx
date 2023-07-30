89d42c13650e1514f53c86c7e35831c88e40fe09
suneelkumar-24 <k163997@nu.edu.pk>
2023-07-30 18:42:20 +0500

    heroku v1.2.4

--- client/src/components/SProductCard.jsx
+++ client/src/components/SProductCard.jsx
@@ -5,7 +5,7 @@ const SProductCard = ({ link,title,image }) => {
   return (
     <Link to={link} ><div className='maxmd:max-w-[330px] maxmd:mx-auto'>
       <div className='flex flex-col items-center justify-center rounded-xl border-[1px] border-gray-200 bg-white p-10' >
-        <img src={image} className='w-56' />
+        <img src={image} className='h-56' />
       </div>
       <h4 className=' font-bold xl:text-xl text-lg mt-2' >{title}</h4>
     </div></Link>
