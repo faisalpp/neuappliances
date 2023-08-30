83e027033af6712813f7a99f27330f1d81376454
suneelkumar-24 <k163997@nu.edu.pk>
2023-08-31 00:14:01 +0500

    neu v1.9.3

--- client/src/components/ReviewSection.jsx
+++ client/src/components/ReviewSection.jsx
@@ -46,8 +46,11 @@ const ReviewSection = () => {
     <div className='flex flex-col justify-center w-full 3xl:max-w-1680px mx-auto xl:px-20 2xl:px-32 lg:px-14 px-4 md:px-10 pb-10 lg:pb-16' >
       <h4 className='text-2xl font-extrabold mb-12' >Saving Austinites Money on Appliances Since 2015</h4>
       <div className='space-y-8'>
-        <ReviewSlider color="#F5F5F5" clientreviews={reviews} icon="google.png" />
-        <ReviewSlider2 color="#ff9b3e14" clientreviews={yelpReviews} icon="yelp.png" />
+      <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
+<div class="elfsight-app-e353e15d-00c0-408e-a088-0d639b57d70f"></div>
+<div class="elfsight-app-a9c6c21c-6d3f-4b27-9d75-ed5d69605c15"></div>
+        {/* <ReviewSlider color="#F5F5F5" clientreviews={reviews} icon="google.png" /> */}
+        {/* <ReviewSlider2 color="#ff9b3e14" clientreviews={yelpReviews} icon="yelp.png" /> */}
       </div>
       <div className='flex justify-center mt-5' ><a href='/' className='flex items-center border-[1px] border-b3 w-fit px-4 py-3 rounded-md text-b3 font-semibold' ><span className='text-sm' >Shop Austin's Best Appliance Deals</span><BsArrowRightShort className='text-2xl' /></a></div>
     </div>
