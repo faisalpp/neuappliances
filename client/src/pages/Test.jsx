926d5df4b42989a4342438231fc1dce73ec513ac
Muhammad Fahad <mfd84739@gmail.com>
2023-09-23 22:04:48 +0500

    added slider

--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -1,18 +1,138 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
-const Test = () => {
+function Test() {
+  const slider1Ref = useRef(null);
+  const slider2Ref = useRef(null);
+  const [nav1, setNav1] = useState(null);
+  const [nav2, setNav2] = useState(null);
+
+
+
+  useEffect(() => {
+    setNav1(slider1Ref.current);
+    setNav2(slider2Ref.current);
+  }, []);
+
+  const settings = {
+    dots: false,
+    arrows: false,
+  };
+
+  const settings2 = {
+    loop: true,
+    infinite: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 575,
+        settings: {
+          arrows: false,
+          dots: true,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          arrows: false,
+          dots: true,
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 5,
+        },
+      },
+      {
+        breakpoint: 1670,
+        settings: {
+          slidesToShow: 6,
+        },
+      },
+    ],
+  };
+  const PrevButton = ({ onClick }) => (
+    <button onClick={onClick} className='hidden sm:block prev-button absolute top-0 -left-10 z-40 h-full pointer-events-none'>
+      <div className='flex carousel__prev bg-black/30 hover:bg-cyan-500 cursor-pointer px-2 py-2 rounded-full text-white group pointer-events-auto'>
+        <BsArrowLeftShort className='text-xl' />
+      </div>
+    </button>
+  );
+
+  const NextButton = ({ onClick }) => (
+    <button onClick={onClick} className='hidden sm:block next-button absolute top-0 -right-10 z-40 h-full pointer-events-none'>
+      <div className='flex carousel__next bg-black/30 hover:bg-cyan-500 cursor-pointer px-2 py-2 rounded-full text-white group pointer-events-auto'>
+        <BsArrowRightShort className='text-xl' />
+      </div>
+    </button>
+  );
   return (
-    <div className='m-10 flex gap-10 h-full'>
-      <div className='w-[30%] bg-red-500 h-full sticky top-10'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam cupiditate, obcaecati nobis voluptates ipsam mollitia officia quas doloribus cumque quos molestiae culpa. Molestiae, facilis suscipit! Expedita mollitia perferendis consectetur aliquid quasi assumenda quo repellat atque, reprehenderit maxime eveniet facere sint suscipit alias, consequuntur, provident deserunt ut repellendus. Quos omnis quas distinctio natus molestiae beatae ut, itaque id placeat quia possimus nisi iure nam odio maxime ea quaerat laudantium labore? Porro, explicabo rerum, deserunt quae nisi sequi, numquam dicta ipsa debitis officiis consectetur quod? Reiciendis vel sapiente repudiandae, repellat unde cum iste ad temporibus, laboriosam deserunt facilis non blanditiis qui quaerat cumque, et magni tempora mollitia suscipit. Deleniti dolor quas molestiae mollitia possimus nesciunt in molestias libero excepturi veritatis! Placeat sit debitis soluta necessitatibus nostrum perspiciatis dolore earum minima, animi voluptatem quia autem dolorem corporis sint voluptas laudantium officia maiores quae velit itaque harum voluptate! Ex natus a iure, deleniti porro magni expedita cum perspiciatis possimus! Ipsa laborum, quia possimus veniam accusamus necessitatibus distinctio illum iste. Tenetur animi quod esse dicta fuga nostrum architecto quibusdam alias quasi ipsum, repellat totam ullam est eius laudantium dolor saepe vero quo sed doloribus ad. Assumenda soluta nam quasi nisi id iusto mollitia eligendi facilis rem quidem numquam commodi, ratione architecto dolor quam voluptate aut ducimus sit quos saepe aperiam minima sequi iure? Doloribus excepturi dolorem fuga rerum adipisci saepe voluptate nam, ex earum soluta blanditiis odio cumque nulla dolor possimus necessitatibus sequi atque optio consectetur quis ipsam non a. Rerum minus cupiditate, accusamus soluta sit beatae voluptas sed fuga repellat praesentium voluptatibus exercitationem numquam dolor sint omnis ipsam vero nemo eligendi? Id fugit pariatur tenetur odit commodi. Quidem non atque tenetur. Quos, incidunt illo dolor commodi veniam iure, exercitationem aliquid temporibus quidem possimus dolorem reiciendis quisquam quam asperiores veritatis at nihil, maiores in nemo!
-      </div>
-      <div className='w-[70%] bg-gray-300 h-full'>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae laborum libero explicabo officiis. Hic veniam consectetur assumenda praesentium inventore deleniti explicabo nihil deserunt dicta. Cumque, magnam. Aliquam, molestiae. Saepe magnam deserunt suscipit dicta excepturi iure quos, laudantium ad, dolore nulla vel at. Veniam neque laboriosam quis incidunt voluptas porro repellat, debitis excepturi possimus. Repellendus error ipsa reprehenderit soluta adipisci. Non consequatur quasi quam cumque! Eos rerum aperiam obcaecati consectetur sequi aliquam repudiandae officia quos repellat dolore eius, earum enim eaque est hic libero. Quia aut debitis aliquam commodi modi vitae totam velit! Dolorem voluptatibus sit tempora iure porro vel, laborum numquam cupiditate eligendi excepturi consequuntur a assumenda! Incidunt laudantium cum iure, earum quisquam illo similique magni sed ducimus, ipsam quaerat facere consectetur sit, eaque cupiditate vero enim ex veniam voluptates totam! Aspernatur minima sit, doloribus repellendus, dolorem quasi aliquam corrupti officiis dolorum quas laborum veritatis magnam asperiores, quidem recusandae nostrum incidunt ea? Voluptatem voluptas veritatis possimus ducimus, assumenda suscipit ratione deleniti, sint, quam omnis sit nobis quasi tempore sunt in impedit sed cumque dolore quae adipisci saepe animi optio. Dolorem modi non ducimus odio quae laboriosam officia quam amet eius, enim molestiae, alias minus, eveniet dignissimos dolores eum quibusdam harum! Natus blanditiis harum, architecto suscipit dicta quae! Vel, dolorem. Harum eum, assumenda quae at, quia, rem saepe asperiores vel minima facere delectus? Veritatis fugit voluptate molestiae fuga fugiat hic consequatur nam, natus saepe tempore libero accusantium ab odio dolores ratione quam tenetur doloribus dolorem maiores rerum consequuntur? Dicta doloribus consectetur animi odio hic laboriosam, at eius aperiam tempora. Dolores laborum ipsum ut corporis sint minima commodi fuga asperiores, dignissimos cum autem, optio magni omnis similique. Quae, inventore quas. Eius earum eum sint reprehenderit laborum sequi quasi perferendis cupiditate ut consectetur nostrum nesciunt, corporis corrupti fugit! Totam aliquam blanditiis vel. Labore eius fugiat laudantium in natus voluptatum vel numquam. Dolor voluptas voluptatum iusto animi esse nisi quibusdam nam temporibus illum ex sed sequi exercitationem hic, quas cupiditate voluptatibus nulla similique. Optio eligendi impedit quo praesentium consequatur tenetur dolor eius aliquid, animi beatae architecto accusantium et dignissimos nihil! Provident quia reiciendis laborum quaerat modi. Rerum, blanditiis. Voluptates perferendis optio qui aliquam, praesentium corporis odit minima consequatur nulla iusto, officiis ea. Cum quam impedit itaque architecto temporibus porro inventore explicabo, error incidunt pariatur consequuntur ad aliquid natus commodi quas? Tempore dolores ipsum, earum velit quas rem a quasi libero accusamus, obcaecati laborum inventore! Saepe, deleniti error? Molestiae quam eos reiciendis numquam, incidunt voluptate reprehenderit, odio quibusdam illum consequuntur dolore harum explicabo. Magni iste illum earum, dolores sed ut quis? Rem fuga fugit ad pariatur, corporis ipsa sed saepe dicta laboriosam quibusdam voluptates? Quos accusamus est magni laudantium non cupiditate nihil voluptas eligendi, repellendus iure, possimus reiciendis corrupti recusandae aspernatur necessitatibus voluptatum quisquam ipsum quis. Vel, aperiam. Eum error nostrum quis similique exercitationem? Nesciunt molestias quo quam eius distinctio, facere corporis nam inventore sint molestiae officia voluptas id consectetur recusandae reiciendis a laborum non cum repudiandae natus optio voluptatem consequatur aperiam. Quasi, excepturi in.
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae laborum libero explicabo officiis. Hic veniam consectetur assumenda praesentium inventore deleniti explicabo nihil deserunt dicta. Cumque, magnam. Aliquam, molestiae. Saepe magnam deserunt suscipit dicta excepturi iure quos, laudantium ad, dolore nulla vel at. Veniam neque laboriosam quis incidunt voluptas porro repellat, debitis excepturi possimus. Repellendus error ipsa reprehenderit soluta adipisci. Non consequatur quasi quam cumque! Eos rerum aperiam obcaecati consectetur sequi aliquam repudiandae officia quos repellat dolore eius, earum enim eaque est hic libero. Quia aut debitis aliquam commodi modi vitae totam velit! Dolorem voluptatibus sit tempora iure porro vel, laborum numquam cupiditate eligendi excepturi consequuntur a assumenda! Incidunt laudantium cum iure, earum quisquam illo similique magni sed ducimus, ipsam quaerat facere consectetur sit, eaque cupiditate vero enim ex veniam voluptates totam! Aspernatur minima sit, doloribus repellendus, dolorem quasi aliquam corrupti officiis dolorum quas laborum veritatis magnam asperiores, quidem recusandae nostrum incidunt ea? Voluptatem voluptas veritatis possimus ducimus, assumenda suscipit ratione deleniti, sint, quam omnis sit nobis quasi tempore sunt in impedit sed cumque dolore quae adipisci saepe animi optio. Dolorem modi non ducimus odio quae laboriosam officia quam amet eius, enim molestiae, alias minus, eveniet dignissimos dolores eum quibusdam harum! Natus blanditiis harum, architecto suscipit dicta quae! Vel, dolorem. Harum eum, assumenda quae at, quia, rem saepe asperiores vel minima facere delectus? Veritatis fugit voluptate molestiae fuga fugiat hic consequatur nam, natus saepe tempore libero accusantium ab odio dolores ratione quam tenetur doloribus dolorem maiores rerum consequuntur? Dicta doloribus consectetur animi odio hic laboriosam, at eius aperiam tempora. Dolores laborum ipsum ut corporis sint minima commodi fuga asperiores, dignissimos cum autem, optio magni omnis similique. Quae, inventore quas. Eius earum eum sint reprehenderit laborum sequi quasi perferendis cupiditate ut consectetur nostrum nesciunt, corporis corrupti fugit! Totam aliquam blanditiis vel. Labore eius fugiat laudantium in natus voluptatum vel numquam. Dolor voluptas voluptatum iusto animi esse nisi quibusdam nam temporibus illum ex sed sequi exercitationem hic, quas cupiditate voluptatibus nulla similique. Optio eligendi impedit quo praesentium consequatur tenetur dolor eius aliquid, animi beatae architecto accusantium et dignissimos nihil! Provident quia reiciendis laborum quaerat modi. Rerum, blanditiis. Voluptates perferendis optio qui aliquam, praesentium corporis odit minima consequatur nulla iusto, officiis ea. Cum quam impedit itaque architecto temporibus porro inventore explicabo, error incidunt pariatur consequuntur ad aliquid natus commodi quas? Tempore dolores ipsum, earum velit quas rem a quasi libero accusamus, obcaecati laborum inventore! Saepe, deleniti error? Molestiae quam eos reiciendis numquam, incidunt voluptate reprehenderit, odio quibusdam illum consequuntur dolore harum explicabo. Magni iste illum earum, dolores sed ut quis? Rem fuga fugit ad pariatur, corporis ipsa sed saepe dicta laboriosam quibusdam voluptates? Quos accusamus est magni laudantium non cupiditate nihil voluptas eligendi, repellendus iure, possimus reiciendis corrupti recusandae aspernatur necessitatibus voluptatum quisquam ipsum quis. Vel, aperiam. Eum error nostrum quis similique exercitationem? Nesciunt molestias quo quam eius distinctio, facere corporis nam inventore sint molestiae officia voluptas id consectetur recusandae reiciendis a laborum non cum repudiandae natus optio voluptatem consequatur aperiam. Quasi, excepturi in.
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae laborum libero explicabo officiis. Hic veniam consectetur assumenda praesentium inventore deleniti explicabo nihil deserunt dicta. Cumque, magnam. Aliquam, molestiae. Saepe magnam deserunt suscipit dicta excepturi iure quos, laudantium ad, dolore nulla vel at. Veniam neque laboriosam quis incidunt voluptas porro repellat, debitis excepturi possimus. Repellendus error ipsa reprehenderit soluta adipisci. Non consequatur quasi quam cumque! Eos rerum aperiam obcaecati consectetur sequi aliquam repudiandae officia quos repellat dolore eius, earum enim eaque est hic libero. Quia aut debitis aliquam commodi modi vitae totam velit! Dolorem voluptatibus sit tempora iure porro vel, laborum numquam cupiditate eligendi excepturi consequuntur a assumenda! Incidunt laudantium cum iure, earum quisquam illo similique magni sed ducimus, ipsam quaerat facere consectetur sit, eaque cupiditate vero enim ex veniam voluptates totam! Aspernatur minima sit, doloribus repellendus, dolorem quasi aliquam corrupti officiis dolorum quas laborum veritatis magnam asperiores, quidem recusandae nostrum incidunt ea? Voluptatem voluptas veritatis possimus ducimus, assumenda suscipit ratione deleniti, sint, quam omnis sit nobis quasi tempore sunt in impedit sed cumque dolore quae adipisci saepe animi optio. Dolorem modi non ducimus odio quae laboriosam officia quam amet eius, enim molestiae, alias minus, eveniet dignissimos dolores eum quibusdam harum! Natus blanditiis harum, architecto suscipit dicta quae! Vel, dolorem. Harum eum, assumenda quae at, quia, rem saepe asperiores vel minima facere delectus? Veritatis fugit voluptate molestiae fuga fugiat hic consequatur nam, natus saepe tempore libero accusantium ab odio dolores ratione quam tenetur doloribus dolorem maiores rerum consequuntur? Dicta doloribus consectetur animi odio hic laboriosam, at eius aperiam tempora. Dolores laborum ipsum ut corporis sint minima commodi fuga asperiores, dignissimos cum autem, optio magni omnis similique. Quae, inventore quas. Eius earum eum sint reprehenderit laborum sequi quasi perferendis cupiditate ut consectetur nostrum nesciunt, corporis corrupti fugit! Totam aliquam blanditiis vel. Labore eius fugiat laudantium in natus voluptatum vel numquam. Dolor voluptas voluptatum iusto animi esse nisi quibusdam nam temporibus illum ex sed sequi exercitationem hic, quas cupiditate voluptatibus nulla similique. Optio eligendi impedit quo praesentium consequatur tenetur dolor eius aliquid, animi beatae architecto accusantium et dignissimos nihil! Provident quia reiciendis laborum quaerat modi. Rerum, blanditiis. Voluptates perferendis optio qui aliquam, praesentium corporis odit minima consequatur nulla iusto, officiis ea. Cum quam impedit itaque architecto temporibus porro inventore explicabo, error incidunt pariatur consequuntur ad aliquid natus commodi quas? Tempore dolores ipsum, earum velit quas rem a quasi libero accusamus, obcaecati laborum inventore! Saepe, deleniti error? Molestiae quam eos reiciendis numquam, incidunt voluptate reprehenderit, odio quibusdam illum consequuntur dolore harum explicabo. Magni iste illum earum, dolores sed ut quis? Rem fuga fugit ad pariatur, corporis ipsa sed saepe dicta laboriosam quibusdam voluptates? Quos accusamus est magni laudantium non cupiditate nihil voluptas eligendi, repellendus iure, possimus reiciendis corrupti recusandae aspernatur necessitatibus voluptatum quisquam ipsum quis. Vel, aperiam. Eum error nostrum quis similique exercitationem? Nesciunt molestias quo quam eius distinctio, facere corporis nam inventore sint molestiae officia voluptas id consectetur recusandae reiciendis a laborum non cum repudiandae natus optio voluptatem consequatur aperiam. Quasi, excepturi in.
-      </div>
+    <div className="w-[90%] mx-auto">
+      <Slider className="w-full" {...settings} ref={slider1Ref} asNavFor={nav2}>
+        <div className="w-full">
+          <iframe src="https://youtube.com/embed/0K0Nx1nG3x4" className="h-[480px] object-cover w-full rounded-2xl" title="W3Schools Free Online Web Tutorials"></iframe>
+        </div>
+        <div className="w-full px-2">
+          <iframe src="https://youtube.com/embed/0K0Nx1nG3x4" className="h-[480px] object-cover w-full rounded-2xl" title="W3Schools Free Online Web Tutorials"></iframe>
+        </div>
+        <div className="w-full px-2">
+          <iframe src="https://youtube.com/embed/0K0Nx1nG3x4" className="h-[480px] object-cover w-full rounded-2xl" title="W3Schools Free Online Web Tutorials"></iframe>
+        </div>
+        <div className="w-full px-2">
+          <iframe src="https://youtube.com/embed/0K0Nx1nG3x4" className="h-[480px] object-cover w-full rounded-2xl" title="W3Schools Free Online Web Tutorials"></iframe>
+        </div>
+        <div className="w-full px-2">
+          <iframe src="https://youtube.com/embed/0K0Nx1nG3x4" className="h-[480px] object-cover w-full rounded-2xl" title="W3Schools Free Online Web Tutorials"></iframe>
+        </div>
+        <div className="w-full px-2">
+          <iframe src="https://youtube.com/embed/0K0Nx1nG3x4" className="h-[480px] object-cover w-full rounded-2xl" title="W3Schools Free Online Web Tutorials"></iframe>
+        </div>
+      </Slider>
+      <Slider {...settings2} className="loop-slider maxmd:pb-10 mt-8 relative" prevArrow={<PrevButton />} nextArrow={<NextButton />} asNavFor={nav1} ref={slider2Ref} swipeToSlide={true} focusOnSelect={true} >
+        <div className="px-1">
+          <img alt="thumbnail" title="thumbnail" src="https://img.youtube.com/vi/OzCAGd4YVbI/mqdefault.jpg" className="h-full w-full rounded-2xl"></img>
+        </div>
+        <div className="px-1">
+          <img alt="thumbnail" title="thumbnail" src="https://img.youtube.com/vi/OzCAGd4YVbI/mqdefault.jpg" className="h-full w-full rounded-2xl"></img>
+        </div>
+        <div className="px-1">
+          <img alt="thumbnail" title="thumbnail" src="https://img.youtube.com/vi/OzCAGd4YVbI/mqdefault.jpg" className="h-full w-full rounded-2xl"></img>
+        </div>
+        <div className="px-1">
+          <img alt="thumbnail" title="thumbnail" src="https://img.youtube.com/vi/OzCAGd4YVbI/mqdefault.jpg" className="h-full w-full rounded-2xl"></img>
+        </div>
+        <div className="px-1">
+          <img alt="thumbnail" title="thumbnail" src="https://img.youtube.com/vi/OzCAGd4YVbI/mqdefault.jpg" className="h-full w-full rounded-2xl"></img>
+        </div>
+        <div className="px-1">
+          <img alt="thumbnail" title="thumbnail" src="https://img.youtube.com/vi/OzCAGd4YVbI/mqdefault.jpg" className="h-full w-full rounded-2xl"></img>
+        </div>
+        <div className="px-1">
+          <img alt="thumbnail" title="thumbnail" src="https://img.youtube.com/vi/OzCAGd4YVbI/mqdefault.jpg" className="h-full w-full rounded-2xl"></img>
+        </div>
+        <div className="px-1">
+          <img alt="thumbnail" title="thumbnail" src="https://img.youtube.com/vi/OzCAGd4YVbI/mqdefault.jpg" className="h-full w-full rounded-2xl"></img>
+        </div>
+      </Slider>
     </div>
-  )
+  );
 }
 
-export default Test
+export default Test;
